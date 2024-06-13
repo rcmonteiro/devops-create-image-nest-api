6647b58f89b8190153ff875c44b1e740a403b3d5
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-06-13 13:39:36 -0300

    chore: remove db from api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'rocketseat-db',
-      entities: [],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'mysql',
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'root',
+    //   database: 'rocketseat-db',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
